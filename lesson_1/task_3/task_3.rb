63bfa28ac92699bed8776934488d53d919a4657f
Chinderbember <chinderbember@yahoo.com>
2022-04-09 15:01:35 +0300

    added lesson 1 demo task_3

---
+++ lesson_1/task_3/task_3.rb
@@ -0,0 +1,17 @@
+lines = File.readlines('test.txt')
+lines_sizes = lines.map(&:size)
+min_size, max_size = lines_sizes.minmax
+result_max = "Максимальная длинна строки: #{max_size}\r\n" 
+result_max += "Строки с максимальной длинной:\r\n"
+
+result_min = "Минимальная длинна строки: #{min_size}\r\n" 
+result_min += "Строки с минимальной длинной:\r\n"
+
+lines.each_with_index do |line, index| 
+	if lines_sizes[index] == max_size
+		result_max += line
+	elsif lines_sizes[index] == min_size
+		result_min += line
+	end
+end
+puts result = result_max + "\r\n" * 2 + result_min
