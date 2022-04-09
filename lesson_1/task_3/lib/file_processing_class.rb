b7ee3a9805e252c28d2f6d7e0bc986cd0bd2d1d8
Chinderbember <chinderbember@yahoo.com>
2022-04-09 15:36:53 +0300

    lesson 1 task_3 final v

--- lesson_1/task_3/lib/file_processing_class.rb
+++ lesson_1/task_3/lib/file_processing_class.rb
@@ -1,21 +1,31 @@
+# frozen_string_literal: true
+
 class FileProcessing
-	def self.minmax_lines(file_path)
-		lines = File.readlines(file_path)
-		lines_sizes = lines.map(&:size)
-		min_size, max_size = lines_sizes.minmax
-		result_max = "Максимальная длинна строки: #{max_size}\r\n" 
-		result_max += "Строки с максимальной длинной:\r\n"
+  def self.minmax_lines(file_path)
+    lines = File.readlines(file_path)
+    lines_sizes = lines.map(&:size)
+    min_size, max_size = lines_sizes.minmax
 
-		result_min = "Минимальная длинна строки: #{min_size}\r\n" 
-		result_min += "Строки с минимальной длинной:\r\n"
+    minmax_lines_arr = minmax_lines_unformatted(lines, lines_sizes, max_size, min_size)
 
-		lines.each_with_index do |line, index| 
-			if lines_sizes[index] == max_size
-				result_max += line
-			elsif lines_sizes[index] == min_size
-				result_min += line
-			end
-		end
-		result = result_max + "\r\n" * 2 + result_min
-	end
+    result_max = "Максимальная длинна строки: #{max_size}\r\n"
+    result_max += "Строки с максимальной длинной:\r\n#{minmax_lines_arr[1]}"
+
+    result_min = "Минимальная длинна строки: #{min_size}\r\n"
+    result_min += "Строки с минимальной длинной:\r\n#{minmax_lines_arr.first}"
+
+    result_max + "\r\n" * 2 + result_min
+  end
+
+  def self.minmax_lines_unformatted(lines, lines_sizes, max_size, min_size)
+    result_min = result_max = ''
+    lines.each_with_index do |line, index|
+      if lines_sizes[index] == max_size
+        result_max += line
+      elsif lines_sizes[index] == min_size
+        result_min += line
+      end
+    end
+    [result_min, result_max]
+  end
 end
--- lesson_1/task_3/task_3.rb
+++ lesson_1/task_3/task_3.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'lib/file_processing_class'
+
 puts FileProcessing.minmax_lines(ARGV.first)
