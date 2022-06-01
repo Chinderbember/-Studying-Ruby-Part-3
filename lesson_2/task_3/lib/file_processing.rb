8a3b3bfe186456f9853921246d980feb87382c0c
Chinderbember <chinderbember@yahoo.com>
2022-06-01 17:12:33 +0300

    added lesson 2 task 3 FINAL_V

--- lesson_2/task_3/lib/file_processing.rb
+++ lesson_2/task_3/lib/file_processing.rb
@@ -1,19 +1,17 @@
+# frozen_string_literal: true
+
 class FileProcessing
+  MBYTES_IN_GBYTE = KBYTES_IN_MBYTE = BYTES_IN_KBYTE = 1024
 
-	MBYTES_IN_GBYTE = KBYTES_IN_MBYTE = BYTES_IN_KBYTE = 1024
-
-	def self.size_format(file_size)
-
-		if (size_format_g = file_size.to_f / BYTES_IN_KBYTE / KBYTES_IN_MBYTE / MBYTES_IN_GBYTE ) >= 1
-			"#{size_format_g}G"
-		elsif (size_format_m = file_size.to_f / BYTES_IN_KBYTE / KBYTES_IN_MBYTE ) >= 1
-			"#{size_format_m}M"
-		elsif (size_format_k = file_size.to_f / BYTES_IN_KBYTE ) >= 1
-			"#{size_format_k}K"
-		else
-			file_size
-		end
-
-	end
-
+  def self.size_format(file_size)
+    if (size_format_g = file_size.to_f / BYTES_IN_KBYTE / KBYTES_IN_MBYTE / MBYTES_IN_GBYTE) >= 1
+      "#{size_format_g.round(2)}G"
+    elsif (size_format_m = file_size.to_f / BYTES_IN_KBYTE / KBYTES_IN_MBYTE) >= 1
+      "#{size_format_m.round(2)}M"
+    elsif (size_format_k = file_size.to_f / BYTES_IN_KBYTE) >= 1
+      "#{size_format_k.round(2)}K"
+    else
+      file_size
+    end
+  end
 end
--- lesson_2/task_3/task_3.rb
+++ lesson_2/task_3/task_3.rb
@@ -1,5 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'lib/file_processing'
 
 puts FileProcessing.size_format(File.size(ARGV.first))
-
-
