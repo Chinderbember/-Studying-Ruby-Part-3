9aca0a3174ff4ee62ae508e641e6eb8942620372
Chinderbember <chinderbember@yahoo.com>
2022-06-05 16:24:08 +0300

    added lesson 3 task_4 v2

--- lesson_3/task_4/task_4.rb
+++ lesson_3/task_4/task_4.rb
@@ -1,15 +1,15 @@
-ROMAN_DIGITS = { 1000 => 'M',
-								 500 => 'D' ,
-								 100 => 'C' ,
-								 50 => 'L' ,
-								 10 => 'X' ,
-								 5 => 'V' ,
-								 1 => 'I'
-								}
+EDINITSY = %w[I II III IV V VI VII VIII IX]
+DESYATKI = %w[X XX XXX XL L LX LXX LXXX XC]
+SOTNI = %w[C CC CCC CD D DC DCC DCCC CM]
+TYSYACHI = %w[M MM MMM]
+SUPPORT_ARR = [EDINITSY, DESYATKI, SOTNI, TYSYACHI]
+
 ARABIC = [1000, 500, 100, 50, 10, 5, 1]
-def roman(number, pos = 0)
+def roman(number, divider = 1000)
 	
-	ROMAN_DIGITS[ARABIC[pos]] * (number / ARABIC[pos] )  +  (pos == 6 ? '' : roman(number - (number / ARABIC[pos]) * ARABIC[pos]  ,pos + 1))
+	result = (number / divider).positive? ? SUPPORT_ARR[Math.log10(divider)][number / divider - 1] : ""
+	result  + 
+	 (divider == 1 ? '' : roman(number - (number / divider) * divider  , divider / 10))
 	
 end
 
