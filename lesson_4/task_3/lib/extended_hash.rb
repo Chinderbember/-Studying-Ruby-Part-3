5535822550d0a1fc4059eefd327692476f9ff19b
Chinderbember <chinderbember@yahoo.com>
2022-06-09 14:59:54 +0300

    lesson 4 task 3 v3

--- lesson_4/task_3/lib/extended_hash.rb
+++ lesson_4/task_3/lib/extended_hash.rb
@@ -1,22 +1,27 @@
 class Hash
-	def to_json_hash(indent = 0)
+	def to_json(indent = 0)
 		"{\r\n" +
-		(to_a.map { |el_fst, el_snd| " " * (indent + 2) + "#{support_met(el_fst, indent + 2)} : #{support_met(el_snd)}"} ).join("\r\n") +
-		+ "\r\n" + ' ' * indent + "}"
+		(to_a.map { |key, value| " " * (indent + 2) + "#{support_met(key, indent + 2)} : #{support_met(value)}"} ).join("\r\n") +
+		"\r\n" + 
+		' ' * indent +
+		"}"
 	end
 
 	def support_met(element, indent = 0)
+
 		if element.respond_to?(:to_str)
 			element
 		elsif element.respond_to?(:to_hash)
-			element.to_json_hash(indent + 2)
+			element.to_json(indent + 2)
 		elsif element.respond_to?(:to_ary)
 			'[' + (element.map { |el| support_met(el, indent + 2)}).join(', ') + ']'
+		# Вообще наверное везде надо было respond_to? сделать, но я подумал, что наврятли кто-то будет выдумывать свой класс вместо чисел, true или false 
 		elsif Numeric === element
 			element
 		elsif FalseClass === element || TrueClass == element
 			element.to_s
 		end
+		
 	end
 
 end
--- lesson_4/task_3/task_3.rb
+++ lesson_4/task_3/task_3.rb
@@ -1,7 +1,4 @@
 require_relative 'lib/extended_hash'
 require 'yaml'
 
-h = {"first_name" => 32321, "last_name" => "Петрович", "patronymic" => "Сидоров", "params" => {"score" => [1, 3, {"last_name" => "Петрович", "patronymic" => "Сидоров"}], "number" => 762 } }
-
-p YAML.load_file('.rubocop.yml')
-puts YAML.load_file('.rubocop.yml').to_json_hash
+puts YAML.load_file('.rubocop.yml').to_json
