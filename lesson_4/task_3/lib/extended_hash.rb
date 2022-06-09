5381596084d6c1a64b06c024cfcba9b745fef506
Chinderbember <chinderbember@yahoo.com>
2022-06-09 15:24:19 +0300

    lesson 4 task 3 final_v

--- lesson_4/task_3/lib/extended_hash.rb
+++ lesson_4/task_3/lib/extended_hash.rb
@@ -1,27 +1,27 @@
+# frozen_string_literal: true
+
 class Hash
-	def to_json(indent = 0)
-		"{\r\n" +
-		(to_a.map { |key, value| " " * (indent + 2) + "#{support_met(key, indent + 2)} : #{support_met(value)}"} ).join("\r\n") +
-		"\r\n" + 
-		' ' * indent +
-		"}"
-	end
+  # rubocop:disable Style/StringConcatenation
+  def to_json(indent = 0)
+    "{\r\n" +
+      (to_a.map do |key, value|
+         ' ' * (indent + 2) + "#{supp_met(key, indent + 2)} : #{supp_met(value)}"
+       end).join("\r\n") +
+      "\r\n" +
+      ' ' * indent +
+      '}'
+  end
 
-	def support_met(element, indent = 0)
-
-		if element.respond_to?(:to_str)
-			element
-		elsif element.respond_to?(:to_hash)
-			element.to_json(indent + 2)
-		elsif element.respond_to?(:to_ary)
-			'[' + (element.map { |el| support_met(el, indent + 2)}).join(', ') + ']'
-		# Вообще наверное везде надо было respond_to? сделать, но я подумал, что наврятли кто-то будет выдумывать свой класс вместо чисел, true или false 
-		elsif Numeric === element
-			element
-		elsif FalseClass === element || TrueClass == element
-			element.to_s
-		end
-		
-	end
-
+  def supp_met(element, indent = 0)
+    if element.respond_to?(:to_hash)
+      element.to_json(indent + 2)
+    elsif element.respond_to?(:to_ary)
+      '[' +
+        (element.map { |el| supp_met(el, indent + 2) }).join(', ') +
+        ']'
+    else
+      element.to_s
+    end
+  end
 end
+# rubocop:enable Style/StringConcatenation
