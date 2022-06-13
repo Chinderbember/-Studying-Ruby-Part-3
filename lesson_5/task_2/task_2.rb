800329dab054806f1d3c1bc7e59c494ff19eaabd
Chinderbember <chinderbember@yahoo.com>
2022-06-13 14:12:13 +0300

    added lesson 5 task_2 v2

--- lesson_5/task_2/task_2.rb
+++ lesson_5/task_2/task_2.rb
@@ -2,7 +2,22 @@ class String
 	def ru_en_alphabet_only?
 		(chars.reject do |char|
 			('a'..'z').include?(char) || ('а'..'я').include?(char)
-		end).empty?
+		end).empty? and (self != '')
 	end
 end
 
+words = []
+10.times do |iteration_num|
+	print "Введите слово №#{iteration_num + 1}:  "
+	word = gets.chomp
+	if word.ru_en_alphabet_only?
+		words << word
+	else
+		puts 'Ошибка! Слово должно состоять только из символов русского и английского алфавита.'
+		redo
+	end
+end
+
+words.each_with_index do |word, index|
+	puts "#{index + 1}. #{word}"
+end
