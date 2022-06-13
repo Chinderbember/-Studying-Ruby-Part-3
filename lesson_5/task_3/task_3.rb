7f20926eef46c8aaac1fd4fb47fea8717cf80861
Chinderbember <chinderbember@yahoo.com>
2022-06-13 18:23:12 +0300

    added lesson 5 task_3 v2

--- lesson_5/task_3/task_3.rb
+++ lesson_5/task_3/task_3.rb
@@ -3,7 +3,8 @@ class Dir
 		content = Dir.entries(path).reject { |el| File.file?(File.join(path, el)) }
 		content.slice!(0..1)
 		content_copy = content.dup
-		content.reduce(content_copy) {|total, el|  total << short_entries(File.join(path, el)) }
+		# content.reduce(content_copy) {|total, el|  total << short_entries(File.join(path, el)) }
+		content.reduce(content_copy) {|total, el|  total + short_entries(File.join(path, el)) }
 	end
 
 	def entries_plus_sub(path)
