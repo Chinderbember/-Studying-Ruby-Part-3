f8eb77e51c6f1798aa2f1dea28c966708401fa6f
Chinderbember <chinderbember@yahoo.com>
2022-06-13 17:46:13 +0300

    added lesson 5 task_3 v1

---
+++ lesson_5/task_3/task_3.rb
@@ -0,0 +1,17 @@
+class Dir
+	def self.short_entries(path)
+		content = Dir.entries(path).reject { |el| File.file?(File.join(path, el)) }
+		content.slice!(0..1)
+		content_copy = content.dup
+		content.reduce(content_copy) {|total, el|  total << short_entries(File.join(path, el)) }
+	end
+
+	def entries_plus_sub(path)
+
+
+	end
+end
+
+p Dir.entries('..')
+p Dir.short_entries('..')
+
