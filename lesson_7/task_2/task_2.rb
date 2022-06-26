7ac144f484ee493021ea21bef29911a1c64ad893
Chinderbember <chinderbember@yahoo.com>
2022-06-26 22:01:31 +0300

    added lesson 7 task 2

---
+++ lesson_7/task_2/task_2.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+COLORS = {
+  red: 'красный',
+  orange: 'оранжевый',
+  yellow: 'желтый',
+  green: 'зеленый',
+  blue: 'голубой',
+  indigo: 'синий',
+  violet: 'фиолетовый'
+}.freeze
+
+p COLORS.values
+p COLORS.keys
+p COLORS.invert
