3dbee636e4d920fd708ab3c7987bd2b0698d0de0
Chinderbember <chinderbember@yahoo.com>
2022-07-06 15:22:01 +0300

    course_work task_1 update

--- course_work/task_1/lib/extended_file.rb
+++ course_work/task_1/lib/extended_file.rb
@@ -1,41 +1,43 @@
 # frozen_string_literal: true
 
 class File
-  def self.divide_on_10_parts(path)
-    error_message = 'Unable to divide file, because file_size < 10 bytes'
-    raise error_message if size(path) < 10
+  class << self
+    def divide_on_10_parts(path)
+      error_message = 'Unable to divide file, because file_size < 10 bytes'
+      raise error_message if size(path) < 10
 
-    divider_10_parts(path)
-  end
+      divider_10_parts(path)
+    end
 
-  def self.divider_10_parts(path)
-    origin_file_size = File.size(path)
-    origin_file_content = File.read(path)
-    parts_amount = 10
+    def divider_10_parts(path)
+      origin_file_size = File.size(path)
+      origin_file_content = File.read(path)
+      parts_amount = 10
 
-    part_size = (origin_file_size.to_f / parts_amount).to_i
-    rest_data_size = origin_file_size - part_size * (parts_amount - 1)
+      part_size = (origin_file_size.to_f / parts_amount).to_i
+      rest_data_size = origin_file_size - part_size * (parts_amount - 1)
 
-    creating_10_parts(path, parts_amount, origin_file_content, indexes_for_parts(part_size, rest_data_size))
-  end
+      creating_10_parts(path, parts_amount, origin_file_content, indexes_for_parts(part_size, rest_data_size))
+    end
 
-  def self.creating_10_parts(path, parts_amount, origin_file_content, indexes_for_parts_var)
-    parts_amount.times do |part_number|
-      part_name = format("#{path}x%02d", part_number + 1)
+    def creating_10_parts(path, parts_amount, origin_file_content, indexes_for_parts_var)
+      parts_amount.times do |part_number|
+        part_name = format("#{path}x%02d", part_number + 1)
 
-      File.open(part_name, 'w') do |created_file|
-        created_file.write origin_file_content.byteslice(indexes_for_parts_var[part_number])
+        File.open(part_name, 'w') do |created_file|
+          created_file.write origin_file_content.byteslice(indexes_for_parts_var[part_number])
+        end
       end
     end
-  end
 
-  def self.indexes_for_parts(part_size, rest_data_size)
-    indexes = Array.new(10) do |part_index|
-      start_index_for_part = 0 + part_index * part_size
-      end_index_for_part = start_index_for_part + part_size - 1
-      start_index_for_part..end_index_for_part
+    def indexes_for_parts(part_size, rest_data_size)
+      indexes = Array.new(10) do |part_index|
+        start_index_for_part = 0 + part_index * part_size
+        end_index_for_part = start_index_for_part + part_size - 1
+        start_index_for_part..end_index_for_part
+      end
+      indexes[-1] = Range.new(indexes[-1].begin, indexes[-1].end + rest_data_size)
+      indexes
     end
-    indexes[-1] = Range.new(indexes[-1].begin, indexes[-1].end + rest_data_size)
-    indexes
   end
 end
